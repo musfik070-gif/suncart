a3f99ec465cd96c756380b6a1c62f5094e37edb1
musfik070-gif <musfik070@gmail.com>
2026-05-03 03:03:13 +0600

    vercel test

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,11 +10,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
-      <body className="min-h-screen flex flex-col">
+      <body className="min-h-screen flex flex-col bg-base-100">
+        {/* Persistent Navbar */}
         <Navbar />
+
+        {/* Page Content */}
         <main className="flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           {children}
         </main>
+
+        {/* Persistent Footer */}
         <Footer />
       </body>
     </html>
