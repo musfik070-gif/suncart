3cadef4bf6dca0a94c45a9b529fcb011cbf3d1ba
musfik070-gif <musfik070@gmail.com>
2026-05-02 13:54:17 +0600

    navbar login and profile logic

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,22 +1,35 @@
+"use client";
+
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const user = localStorage.getItem("isLoggedIn");
+    setLoggedIn(!!user);
+  }, []);
+
   return (
     <div className="navbar bg-base-100 shadow-md px-6">
       <div className="flex-1">
-        <Link href="/" className="text-2xl font-bold text-primary">
+        <Link href="/" className="text-3xl font-bold text-primary">
           SunCart
         </Link>
       </div>
 
-      <div className="flex gap-4 items-center">
+      <div className="flex gap-5 items-center">
         <Link href="/">Home</Link>
         <Link href="/products">Products</Link>
-        <Link href="/profile">My Profile</Link>
 
-        <Link href="/login" className="btn btn-sm btn-primary">
-          Login
-        </Link>
+        {loggedIn && <Link href="/profile">My Profile</Link>}
+
+        {!loggedIn && (
+          <Link href="/login" className="btn btn-sm btn-primary">
+            Login
+          </Link>
+        )}
       </div>
     </div>
   );
